036a8aff841b9eb09cec51999d87e34aeff327f7
0xOrigin <egyahmed.ezzat120@gmail.com>
2025-09-24 00:59:11 +0300

    Add Wrappers javadocs

--- src/main/java/io/github/_0xorigin/queryfilterbuilder/base/wrappers/FilterErrorWrapper.java
+++ src/main/java/io/github/_0xorigin/queryfilterbuilder/base/wrappers/FilterErrorWrapper.java
@@ -2,6 +2,13 @@ package io.github._0xorigin.queryfilterbuilder.base.wrappers;
 
 import org.springframework.validation.BindingResult;
 
+/**
+ * A wrapper that holds the {@link BindingResult} and the {@link FilterWrapper}
+ * currently being processed. This is used to provide context when reporting errors.
+ *
+ * @param bindingResult The Spring {@link BindingResult} instance where validation errors are collected.
+ * @param filterWrapper The filter wrapper that is being processed when the error occurs.
+ */
 public record FilterErrorWrapper(
     BindingResult bindingResult,
     FilterWrapper filterWrapper
--- src/main/java/io/github/_0xorigin/queryfilterbuilder/base/wrappers/SortErrorWrapper.java
+++ src/main/java/io/github/_0xorigin/queryfilterbuilder/base/wrappers/SortErrorWrapper.java
@@ -2,6 +2,13 @@ package io.github._0xorigin.queryfilterbuilder.base.wrappers;
 
 import org.springframework.validation.BindingResult;
 
+/**
+ * A wrapper that holds the {@link BindingResult} and the {@link SortWrapper}
+ * currently being processed. This is used to provide context when reporting errors.
+ *
+ * @param bindingResult The Spring {@link BindingResult} instance where validation errors are collected.
+ * @param sortWrapper   The sort wrapper that is being processed when the error occurs.
+ */
 public record SortErrorWrapper(
     BindingResult bindingResult,
     SortWrapper sortWrapper
