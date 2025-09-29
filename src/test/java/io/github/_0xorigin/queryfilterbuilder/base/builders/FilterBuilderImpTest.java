7240d5f45845391aa7d24578c8d0d87e282d60e0
0xOrigin <egyahmed.ezzat120@gmail.com>
2025-09-30 00:53:30 +0300

    Add unit tests for lastest version of FilterBuilderImp

--- src/test/java/io/github/_0xorigin/queryfilterbuilder/base/builders/FilterBuilderImpTest.java
+++ src/test/java/io/github/_0xorigin/queryfilterbuilder/base/builders/FilterBuilderImpTest.java
@@ -3,18 +3,21 @@ package io.github._0xorigin.queryfilterbuilder.base.builders;
 import io.github._0xorigin.queryfilterbuilder.FilterContext;
 import io.github._0xorigin.queryfilterbuilder.QueryFilterBuilderImp;
 import io.github._0xorigin.queryfilterbuilder.base.dtos.FilterRequest;
+import io.github._0xorigin.queryfilterbuilder.base.enumfield.AbstractEnumFilterField;
 import io.github._0xorigin.queryfilterbuilder.base.enums.FilterType;
 import io.github._0xorigin.queryfilterbuilder.base.enums.MessageKey;
 import io.github._0xorigin.queryfilterbuilder.base.enums.SourceType;
 import io.github._0xorigin.queryfilterbuilder.base.filterfield.AbstractFilterField;
 import io.github._0xorigin.queryfilterbuilder.base.filteroperator.FilterOperator;
 import io.github._0xorigin.queryfilterbuilder.base.filteroperator.Operator;
+import io.github._0xorigin.queryfilterbuilder.base.functions.CustomFilterFunction;
 import io.github._0xorigin.queryfilterbuilder.base.generators.PathGenerator;
 import io.github._0xorigin.queryfilterbuilder.base.holders.CustomFilterHolder;
 import io.github._0xorigin.queryfilterbuilder.base.holders.ErrorHolder;
 import io.github._0xorigin.queryfilterbuilder.base.holders.FilterHolder;
 import io.github._0xorigin.queryfilterbuilder.base.parsers.FilterParser;
 import io.github._0xorigin.queryfilterbuilder.base.services.LocalizationService;
+import io.github._0xorigin.queryfilterbuilder.base.utils.FilterUtils;
 import io.github._0xorigin.queryfilterbuilder.base.wrappers.FilterErrorWrapper;
 import io.github._0xorigin.queryfilterbuilder.base.wrappers.FilterWrapper;
 import io.github._0xorigin.queryfilterbuilder.entities.User;
@@ -54,6 +57,9 @@ class FilterBuilderImpTest {
 
     @Mock
     private FilterOperatorRegistry filterOperatorRegistry;
+
+    @Mock
+    private AbstractEnumFilterField enumFilterField;
 
     @Mock
     private LocalizationService localizationService;
@@ -96,6 +102,10 @@ class FilterBuilderImpTest {
 
     @InjectMocks
     private FilterBuilderImp<User> filterBuilder;
+
+    private enum TestEnum {
+        VALUE1, VALUE2
+    }
 
     @Test
     void getDistinctFilterWrappers_WithHttpServletRequest_ReturnsParsedWrappers() {
@@ -412,6 +422,147 @@ class FilterBuilderImpTest {
         assertThatThrownBy(() ->
             filterBuilder.buildPredicateForWrapper(root, criteriaQuery, criteriaBuilder, filterContext, wrapper, errorHolder))
             .isInstanceOf(QueryBuilderConfigurationException.class);
+    }
+
+    @Test
+    void getDistinctFilterWrappers_WithBothSources_BodyOverridesQuery() {
+        // Arrange
+        when(filterContext.getRequest()).thenReturn(Optional.of(httpServletRequest));
+        when(filterContext.getFilterRequests()).thenReturn(Optional.of(List.of(new FilterRequest("firstName", Operator.EQ.name(), "Jane"))));
+
+        FilterWrapper queryWrapper = new FilterWrapper("firstName", "firstName", Operator.EQ, List.of("John"), SourceType.QUERY_PARAM, Optional.empty());
+        when(filterParser.parse(httpServletRequest)).thenReturn(List.of(queryWrapper));
+
+        FilterWrapper bodyWrapper = new FilterWrapper("firstName", "firstName", Operator.EQ, List.of("Jane"), SourceType.REQUEST_BODY, Optional.empty());
+        when(filterParser.parse(anyList())).thenReturn(List.of(bodyWrapper));
+
+        FilterHolder<User, String> filterHolder = mock(FilterHolder.class);
+        when(filterContext.getFilters()).thenReturn(Map.of("firstName", filterHolder));
+        when(filterHolder.operators()).thenReturn(Set.of(Operator.EQ));
+        when(filterHolder.sourceTypes()).thenReturn(Set.of(SourceType.QUERY_PARAM, SourceType.REQUEST_BODY));
+
+        // Act
+        Collection<FilterWrapper> result = filterBuilder.getDistinctFilterWrappers(filterContext);
+
+        // Assert
+        assertThat(result).hasSize(1);
+        FilterWrapper resWrapper = result.iterator().next();
+        assertThat(resWrapper.values()).containsExactly("Jane");
+        assertThat(resWrapper.sourceType()).isEqualTo(SourceType.REQUEST_BODY);
+        assertThat(resWrapper.filterType()).contains(FilterType.NORMAL);
+        verify(filterParser).parse(httpServletRequest);
+        verify(filterParser).parse(anyList());
+    }
+
+    @Test
+    void buildPredicateForWrapper_NormalFilterType_ValidEnumFilter_ReturnsPredicate() {
+        // Arrange
+        List<String> values = List.of("VALUE1");
+        FilterWrapper wrapper = new FilterWrapper("enumField", "enumField", Operator.EQ, values, SourceType.QUERY_PARAM, Optional.of(FilterType.NORMAL));
+        FilterHolder<User, TestEnum> filterHolder = mock(FilterHolder.class);
+        when(filterContext.getFilters()).thenReturn(Map.of("enumField", filterHolder));
+        when(filterHolder.operators()).thenReturn(Set.of(Operator.EQ));
+        when(filterHolder.sourceTypes()).thenReturn(Set.of(SourceType.QUERY_PARAM));
+        Expression<TestEnum> enumExpression = mock(Expression.class);
+        when(filterHolder.getExpression(root, criteriaQuery, criteriaBuilder)).thenAnswer(invocation -> Optional.of(enumExpression));
+        when(enumExpression.getJavaType()).thenAnswer(invocation -> TestEnum.class);
+        when(filterOperatorRegistry.getOperator(Operator.EQ)).thenReturn(filterOperator);
+        when(enumFilterField.getSupportedOperators()).thenReturn(Set.of(Operator.EQ));
+        when(enumFilterField.safeCast(eq(TestEnum.class), eq("VALUE1"), any(FilterErrorWrapper.class))).thenReturn(TestEnum.VALUE1);
+        List<TestEnum> enumValues = List.of(TestEnum.VALUE1);
+        when(filterOperator.apply(eq(enumExpression), eq(criteriaBuilder), eq(enumValues), any(FilterErrorWrapper.class))).thenReturn(Optional.of(predicate));
+        when(errorHolder.bindingResult()).thenReturn(bindingResult);
+
+        // Act
+        Optional<Predicate> result = filterBuilder.buildPredicateForWrapper(root, criteriaQuery, criteriaBuilder, filterContext, wrapper, errorHolder);
+
+        // Assert
+        assertThat(result).isPresent().contains(predicate);
+        verify(enumFilterField).safeCast(eq(TestEnum.class), eq("VALUE1"), any());
+        verify(filterOperator).apply(eq(enumExpression), eq(criteriaBuilder), eq(enumValues), any());
+    }
+
+    @Test
+    void buildPredicateForWrapper_CustomFilterType_ValidEnumFilter_ReturnsPredicate() {
+        // Arrange
+        List<String> values = List.of("VALUE1");
+        FilterWrapper wrapper = new FilterWrapper("enumField", "enumField", Operator.EQ, values, SourceType.REQUEST_BODY, Optional.of(FilterType.CUSTOM));
+        CustomFilterHolder<User, TestEnum> customFilterHolder = mock(CustomFilterHolder.class);
+        when(filterContext.getCustomFilters()).thenReturn(Map.of("enumField", customFilterHolder));
+        when(customFilterHolder.sourceTypes()).thenReturn(Set.of(SourceType.REQUEST_BODY));
+        when(customFilterHolder.dataType()).thenReturn(TestEnum.class);
+        when(filterOperatorRegistry.getOperator(Operator.EQ)).thenReturn(filterOperator);
+        when(enumFilterField.getSupportedOperators()).thenReturn(Set.of(Operator.EQ));
+        when(enumFilterField.safeCast(eq(TestEnum.class), eq("VALUE1"), any(FilterErrorWrapper.class))).thenReturn(TestEnum.VALUE1);
+        when(customFilterHolder.customFilterFunction()).thenReturn(mock(CustomFilterFunction.class));
+        when(customFilterHolder.customFilterFunction().apply(any(), any(), any(), any(), any())).thenAnswer(invocation -> Optional.of(predicate));
+        when(errorHolder.bindingResult()).thenReturn(bindingResult);
+
+        // Act
+        Optional<Predicate> result = filterBuilder.buildPredicateForWrapper(root, criteriaQuery, criteriaBuilder, filterContext, wrapper, errorHolder);
+
+        // Assert
+        assertThat(result).isPresent().contains(predicate);
+        verify(enumFilterField).safeCast(eq(TestEnum.class), eq("VALUE1"), any());
+        verify(customFilterHolder.customFilterFunction()).apply(any(), any(), any(), any(), any());
+    }
+
+    @Test
+    void buildPredicateForWrapper_NormalFilterType_UnsupportedOperatorForEnum_ThrowsException() {
+        // Arrange
+        List<String> values = List.of("VALUE1");
+        FilterWrapper wrapper = new FilterWrapper("enumField", "enumField", Operator.GT, values, SourceType.QUERY_PARAM, Optional.of(FilterType.NORMAL));
+        FilterHolder<User, TestEnum> filterHolder = mock(FilterHolder.class);
+        when(filterContext.getFilters()).thenReturn(Map.of("enumField", filterHolder));
+        when(filterHolder.operators()).thenReturn(Set.of(Operator.GT));
+        when(filterHolder.sourceTypes()).thenReturn(Set.of(SourceType.QUERY_PARAM));
+        Expression<TestEnum> enumExpression = mock(Expression.class);
+        when(filterHolder.getExpression(root, criteriaQuery, criteriaBuilder)).thenAnswer(invocation -> Optional.of(enumExpression));
+        when(errorHolder.bindingResult()).thenReturn(bindingResult);
+        when(errorHolder.methodParameter()).thenReturn(getFilterMethodParameter());
+        when(bindingResult.hasErrors()).thenReturn(true);
+
+        // Act & Assert
+        assertThatThrownBy(() -> filterBuilder.buildPredicateForWrapper(root, criteriaQuery, criteriaBuilder, filterContext, wrapper, errorHolder))
+            .isInstanceOf(QueryBuilderConfigurationException.class);
+    }
+
+    @Test
+    void buildPredicateForWrapper_NormalFilterType_InvalidEnumValue_AddsError() {
+        // Arrange
+        List<String> values = List.of("INVALID");
+        FilterWrapper wrapper = new FilterWrapper("enumField", "enumField", Operator.EQ, values, SourceType.QUERY_PARAM, Optional.of(FilterType.NORMAL));
+        FilterHolder<User, TestEnum> filterHolder = mock(FilterHolder.class);
+        when(filterContext.getFilters()).thenReturn(Map.of("enumField", filterHolder));
+        when(filterHolder.operators()).thenReturn(Set.of(Operator.EQ));
+        when(filterHolder.sourceTypes()).thenReturn(Set.of(SourceType.QUERY_PARAM));
+        Expression<TestEnum> enumExpression = mock(Expression.class);
+        when(filterHolder.getExpression(root, criteriaQuery, criteriaBuilder)).thenAnswer(invocation -> Optional.of(enumExpression));
+        when(enumExpression.getJavaType()).thenAnswer(invocation -> TestEnum.class);
+        when(filterOperatorRegistry.getOperator(Operator.EQ)).thenReturn(filterOperator);
+        when(enumFilterField.getSupportedOperators()).thenReturn(Set.of(Operator.EQ));
+        when(enumFilterField.safeCast(eq(TestEnum.class), eq("INVALID"), any(FilterErrorWrapper.class))).thenAnswer(invocation -> {
+            FilterErrorWrapper err = invocation.getArgument(2);
+            FilterUtils.addFieldError(
+                err.bindingResult(),
+                err.filterWrapper().originalFieldName(),
+                "INVALID",
+                "Not a valid enum value"
+            );
+            return null;
+        });
+        List<TestEnum> enumValues = Collections.singletonList(null);
+        when(filterOperator.apply(eq(enumExpression), eq(criteriaBuilder), eq(enumValues), any(FilterErrorWrapper.class))).thenReturn(Optional.empty());
+        when(errorHolder.bindingResult()).thenReturn(bindingResult);
+        when(bindingResult.getObjectName()).thenReturn("filter");
+
+        // Act
+        Optional<Predicate> result = filterBuilder.buildPredicateForWrapper(root, criteriaQuery, criteriaBuilder, filterContext, wrapper, errorHolder);
+
+        // Assert
+        assertThat(result).isEmpty();
+        verify(bindingResult).addError(any());
+        verify(filterOperator).apply(eq(enumExpression), eq(criteriaBuilder), eq(enumValues), any());
     }
 
     private MethodParameter getFilterMethodParameter() {
